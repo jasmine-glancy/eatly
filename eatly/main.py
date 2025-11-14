8c1a57b98e76c3731182cb653fcd2d057898125a
Jasmine <147888618+jasmine-glancy@users.noreply.github.com>
2025-11-13 22:47:29 -0500

    Add ask_page route to Flask application
    Add the ask page in preparation for the meal recommendation page

--- eatly/main.py
+++ eatly/main.py
@@ -3,15 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Home page
-
-# Meal Recommendation Page/Ask Page
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# Meal Recommendation Page/Ask Page
+@app.route("/ask_page")
+def ask_page():
+    return render_template("ask_page.html")
 
 # Community / Feed Page
 
 # Request a Home-Cooked Meal (Optional)
 
 # About / Contact (Optional)
+
 
